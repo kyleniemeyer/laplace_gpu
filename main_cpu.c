3a726088bcea0ce4ac19dc46daa7192f7203a444
Kyle Niemeyer <kyle.niemeyer@gmail.com>
2012-09-21 15:23:22 -0400

    Added comments and optimized CPU version to avoid unnecessary iterations

--- main_cpu.c
+++ main_cpu.c
@@ -1,10 +1,25 @@
+/** CPU Laplace solver using optimized red-black Gauss–Seidel with SOR solver
+ * \file main_cpu_opt.c
+ *
+ * \author Kyle E. Niemeyer
+ * \date 09/21/2012
+ *
+ * Solves Laplace's equation in 2D (e.g., heat conduction in a rectangular plate)
+ * using the red-black Gauss–Seidel with sucessive overrelaxation (SOR) that has
+ * been "optimized". This means that the red and black kernels only loop over
+ * their respective cells, instead of over all cells and skipping even/odd cells.
+ * 
+ * Boundary conditions:
+ * T = 0 at x = 0, x = L, y = 0
+ * T = TN at y = H
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-
 #include <time.h>
-//#include "walltime.h"
 
+/** Double precision */
 #define Real double
 
 typedef unsigned int uint;
@@ -12,6 +27,25 @@ typedef unsigned int uint;
 /** SOR relaxation parameter */
 const Real omega = 1.85;
 
+/** Problem size along one side; total number of cells is this squared */
+#define NUM 8192
+
+/** Function to evaluate coefficient matrix and right-hand side vector.
+ * 
+ * \param[in]		rowmax		number of rows
+ * \param[in]		colmax		number of columns
+ * \param[in]		th_cond		thermal conductivity
+ * \param[in]		dx				grid size in x dimension (uniform)
+ * \param[in]		dy				grid size in y dimension (uniform)
+ * \param[in]		width			width of plate (z dimension)
+ * \param[in]		TN				temperature at top boundary
+ * \param[out]	aP				array of self coefficients
+ * \param[out]	aW				array of west neighbor coefficients
+ * \param[out]	aE				array of east neighbor coefficients
+ * \param[out]	aS				array of south neighbor coefficients
+ * \param[out]	aN				array of north neighbor coefficients
+ * \param[out]	b					right-hand side array
+ */
 void fill_coeffs (uint rowmax, uint colmax, Real th_cond, Real dx, Real dy,
 									Real width, Real TN, Real * aP, Real * aW, Real * aE, 
 									Real * aS, Real * aN, Real * b) {
@@ -60,56 +94,84 @@ void fill_coeffs (uint rowmax, uint colmax, Real th_cond, Real dx, Real dy,
 			
 		} // end for row
 	} // end for col
-}
+} // end fill_coeffs
 
-void red_kernel (uint rowmax, uint colmax, const Real * aP, const Real * aW, const Real * aE, 
-								 const Real * aS, const Real * aN, const Real * b, Real * temp)
+///////////////////////////////////////////////////////////////////////////////
+
+/** Function to update temperature for red cells
+ * 
+ * \param[in]			aP					array of self coefficients
+ * \param[in]			aW					array of west neighbor coefficients
+ * \param[in]			aE					array of east neighbor coefficients
+ * \param[in]			aS					array of south neighbor coefficients
+ * \param[in]			aN					array of north neighbor coefficients
+ * \param[in]			b						right-hand side array
+ * \param[in]			temp_black	temperatures of black cells, constant in this function
+ * \param[inout]	temp_red		temperatures of red cells
+ */
+void red_kernel (const Real * aP, const Real * aW, const Real * aE,
+								 const Real * aS, const Real * aN, const Real * b,
+								 const Real * temp_black, Real * temp_red)
 {
-	for (uint col = 1; col < colmax - 1; ++col) {
-		for (uint row = 1; row < rowmax - 1; ++row) {
+	// loop over actual cells, skip boundary cells
+	for (uint col = 1; col < NUM + 1; ++col) {
+		for (uint row = 1; row < (NUM / 2) + 1; ++row) {
 			
-			// only red cell if even
-			if ((row + col) % 2 == 0) {
-				uint ind = (col - 1) * (rowmax - 2) + (row - 1);
-				uint ind2 = col * rowmax + row;
-				
-				Real res = b[ind] + (aW[ind] * temp[(col - 1) * rowmax + row]
-													 + aE[ind] * temp[(col + 1) * rowmax + row]
-													 + aS[ind] * temp[col * rowmax + (row - 1)]
-													 + aN[ind] * temp[col * rowmax + (row + 1)]);
-				
-				temp[ind2] = temp[ind2] * (1.0 - omega) + omega * (res / aP[ind]);
-			}	
+			uint ind_red = col * ((NUM / 2) + 2) + row;  		// local (red) index
+			uint ind = 2 * row - (col % 2) - 1 + NUM * (col - 1);	// global index
+			
+			Real res = b[ind] + (aW[ind] * temp_black[row + (col - 1) * ((NUM / 2) + 2)]
+											   + aE[ind] * temp_black[row + (col + 1) * ((NUM / 2) + 2)]
+											   + aS[ind] * temp_black[row - (col % 2) + col * ((NUM / 2) + 2)]
+											   + aN[ind] * temp_black[row + ((col + 1) % 2) + col * ((NUM / 2) + 2)]);
+			
+			temp_red[ind_red] = temp_red[ind_red] * (1.0 - omega) + omega * (res / aP[ind]);
 				
 		} // end for row
-	} // end for col
-	
-}
+	} // end for col	
+} // end red_kernel
 
-void black_kernel (uint rowmax, uint colmax, const Real * aP, const Real * aW, const Real * aE, 
-								   const Real * aS, const Real * aN, const Real * b, Real * temp)
+///////////////////////////////////////////////////////////////////////////////
+
+/** Function to update temperature for black cells
+ * 
+ * \param[in]			aP					array of self coefficients
+ * \param[in]			aW					array of west neighbor coefficients
+ * \param[in]			aE					array of east neighbor coefficients
+ * \param[in]			aS					array of south neighbor coefficients
+ * \param[in]			aN					array of north neighbor coefficients
+ * \param[in]			b						right-hand side array
+ * \param[in]			temp_red		temperatures of red cells, constant in this function
+ * \param[inout]	temp_black	temperatures of black cells
+ */
+void black_kernel (const Real * aP, const Real * aW, const Real * aE,
+								   const Real * aS, const Real * aN, const Real * b,
+									 const Real * temp_red, Real * temp_black)
 {
-	for (uint col = 1; col < colmax - 1; ++col) {
-		for (uint row = 1; row < rowmax - 1; ++row) {
+	// loop over actual cells, skip boundary cells
+	for (uint col = 1; col < NUM + 1; ++col) {
+		for (uint row = 1; row < (NUM / 2) + 1; ++row) {
 			
-			// only black cell if odd
-			if ((row + col) % 2 == 1) {
-				uint ind = (col - 1) * (rowmax - 2) + (row - 1);
-				uint ind2 = col * rowmax + row;
-				
-				Real res = b[ind] + (aW[ind] * temp[(col - 1) * rowmax + row]
-													 + aE[ind] * temp[(col + 1) * rowmax + row]
-													 + aS[ind] * temp[col * rowmax + (row - 1)]
-													 + aN[ind] * temp[col * rowmax + (row + 1)]);
-				
-				temp[ind2] = temp[ind2] * (1.0 - omega) + omega * (res / aP[ind]);	
-			}
-				
+			uint ind_black = col * ((NUM / 2) + 2) + row;  					// local (black) index
+			uint ind = 2 * row - ((col + 1) % 2) - 1 + NUM * (col - 1);	// global index
+
+			Real res = b[ind] + (aW[ind] * temp_red[row + (col - 1) * ((NUM / 2) + 2)]
+											   + aE[ind] * temp_red[row + (col + 1) * ((NUM / 2) + 2)]
+											   + aS[ind] * temp_red[row - ((col + 1) % 2) + col * ((NUM / 2) + 2)]
+											   + aN[ind] * temp_red[row + (col % 2) + col * ((NUM / 2) + 2)]);
+			
+			temp_black[ind_black] = temp_black[ind_black] * (1.0 - omega) + omega * (res / aP[ind]);
+			
 		} // end for row
 	} // end for col
-	
-}
+} // end black_kernel
 
+///////////////////////////////////////////////////////////////////////////////
+
+/** Main function that solves Laplace's equation in 2D (heat conduction in plate)
+ * 
+ * Contains iteration loop for red-black Gauss-Seidel with SOR
+ */
 int main (void) {
 	
 	// size of plate
@@ -128,21 +190,23 @@ int main (void) {
 	
 	// number of cells in x and y directions
 	// including unused boundary cells
-	uint num_rows = 4096 + 2;
-	uint num_cols = 4096 + 2;
+	uint num_rows = (NUM / 2) + 2;
+	uint num_cols = NUM + 2;
 	uint size_temp = num_rows * num_cols;
-	uint size = (num_rows - 2) * (num_cols - 2);
+	uint size = NUM * NUM;
 	
 	// size of cells
-	Real dx = L / (num_rows - 2);
-	Real dy = H / (num_cols - 2);
+	Real dx = L / NUM;
+	Real dy = H / NUM;
 	
 	// iterations for Red-Black Gauss-Seidel with SOR
 	uint iter;
 	uint it_max = 1e6;
 	
 	// allocate memory
-	Real *aP, *aW, *aE, *aS, *aN, *b, *temp, *temp_old;
+	Real *aP, *aW, *aE, *aS, *aN, *b;
+	Real *temp_red, *temp_red_old;
+	Real *temp_black, *temp_black_old;
 	
 	// arrays of coefficients
 	aP = (Real *) calloc (size, sizeof(Real));
@@ -154,23 +218,21 @@ int main (void) {
 	// RHS
 	b = (Real *) calloc (size, sizeof(Real));
 	
-	// temperature
-	temp = (Real *) calloc (size_temp, sizeof(Real));
-	temp_old = (Real *) calloc (size_temp, sizeof(Real));
+	// temperature arrays for red and black cells
+	temp_red = (Real *) calloc (size_temp, sizeof(Real));
+	temp_red_old = (Real *) calloc (size_temp, sizeof(Real));
+	temp_black = (Real *) calloc (size_temp, sizeof(Real));
+	temp_black_old = (Real *) calloc (size_temp, sizeof(Real));
 	
 	// set coefficients
-	fill_coeffs (num_rows - 2, num_cols - 2, th_cond, dx, dy, width, TN, aP, aW, aE, aS, aN, b);
+	fill_coeffs (NUM, NUM, th_cond, dx, dy, width, TN, aP, aW, aE, aS, aN, b);
 	
 	for (uint i = 0; i < size_temp; ++i) {
-		temp[i] = 0.0;
-		temp_old[i] = 0.0;
+		temp_red[i] = 0.0;
+		temp_red_old[i] = 0.0;
+		temp_black[i] = 0.0;
+		temp_black_old[i] = 0.0;
 	}
-	
-	/*
-	for (uint i = 0; i < size; ++i) {
-		printf("%i  %f %f %f %f %f %f\n", i, aP[i], aW[i], aE[i], aS[i], aN[i], b[i]);
-	}
-	*/
 	
 	//////////////////////////////
 	// start timer
@@ -179,21 +241,30 @@ int main (void) {
 	clock_t start_time = clock();
 	//////////////////////////////
 	
-	// iteration loop
+	// red-black Gauss-Seidel with SOR iteration loop
 	for (iter = 1; iter <= it_max; ++iter) {
 		
+		Real norm_L2 = 0.0;
+		
 		// update red cells
-		red_kernel (num_rows, num_cols, aP, aW, aE, aS, aN, b, temp);
+		red_kernel (aP, aW, aE, aS, aN, b, temp_black, temp_red);
+		
+		// add red contribution to residual
+		for (uint i = 0; i < size_temp; ++i) {
+			norm_L2 += (temp_red[i] - temp_red_old[i]) * (temp_red[i] - temp_red_old[i]);
+			temp_red_old[i] = temp_red[i];
+		}
 		
 		// update black cells
-		black_kernel (num_rows, num_rows, aP, aW, aE, aS, aN, b, temp);
+		black_kernel (aP, aW, aE, aS, aN, b, temp_red, temp_black);
 		
-		// check residual to see if done with iterations
-		Real norm_L2 = 0.0;
+		// add black contribution to residual
 		for (uint i = 0; i < size_temp; ++i) {
-			norm_L2 += (temp[i] - temp_old[i]) * (temp[i] - temp_old[i]);
-			temp_old[i] = temp[i];
+			norm_L2 += (temp_black[i] - temp_black_old[i]) * (temp_black[i] - temp_black_old[i]);
+			temp_black_old[i] = temp_black[i];
 		}
+		
+		// calculate residual
 		norm_L2 = sqrt(norm_L2 / (size));
 		
 		// if tolerance has been reached, end SOR iterations
@@ -211,31 +282,44 @@ int main (void) {
 	printf("CPU\nIterations: %i\n", iter);
 	printf("Time: %f\n", (end_time - start_time) / (double)CLOCKS_PER_SEC);
 	
+	// write temperature data to file
 	FILE * pfile;
 	pfile = fopen("temp_cpu.dat", "w");
 	if (pfile != NULL) {
 		fprintf(pfile, "#x\ty\ttemp(K)\n");
 		
-		for (uint row = 1; row < num_rows - 1; ++row) {
-			for (uint col = 1; col < num_cols - 1; ++col) {
-				uint ind = col * num_rows + row;
+		for (uint row = 1; row < NUM + 1; ++row) {
+			for (uint col = 1; col < NUM + 1; ++col) {
 				Real x_pos = (col - 1) * dx + (dx / 2);
 				Real y_pos = (row - 1) * dy + (dy / 2);
-				fprintf(pfile, "%f\t%f\t%f\n", x_pos, y_pos, temp[ind]);
+				
+				if ((row + col) % 2 == 0) {
+					// even, so red cell
+					uint ind = col * num_rows + (row + (col % 2)) / 2;
+					fprintf(pfile, "%f\t%f\t%f\n", x_pos, y_pos, temp_red[ind]);
+				} else {
+					// odd, so black cell
+					uint ind = col * num_rows + (row + ((col + 1) % 2)) / 2;
+					fprintf(pfile, "%f\t%f\t%f\n", x_pos, y_pos, temp_black[ind]);
+				}
+				
 			}
 			fprintf(pfile, "\n");
 		}
 	}
 	fclose(pfile);
 	
+	// free memory
 	free(aP);
 	free(aW);
 	free(aE);
 	free(aS);
 	free(aN);
 	free(b);
-	free(temp);
-	free(temp_old);
+	free(temp_red);
+	free(temp_red_old);
+	free(temp_black);
+	free(temp_black_old);
 	
 	return 0;
 }
